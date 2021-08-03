0a0cf4915c66607ec1aa649fafc5525733e44649
NRue <nrue@daytonfreight.com>
2021-08-03 16:29:00 -0400

    add _app, change css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Developer Bio</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       Hello World!!!.{" "}
       <Link href="/about">
         <a>About</a>
