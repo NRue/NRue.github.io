f049d35060c68c04192b568a65fa44114dd8ece2
NRue <nrue@daytonfreight.com>
2021-08-03 14:46:32 -0400

    Deploy Next.js to gh-pages

---
+++ out/_next/static/9LqlF2zTtTouceTSHB-Ka/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b64363171969c30f4b0e.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],"/about":["static/chunks/pages/about-5bee9595a42f18dbf5b3.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
